7a03467dfcb22507bb3db99f7658ea281ba08780
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-06-22 19:21:09 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(){
   const buttons = document.querySelectorAll('[data-tab-button]');
+  const quetions = document.querySelectorAll('[data-faq-question]')
+  const herosSection = document.querySelector('.hero');
+  const alturaHero = herosSection.clientHeight;
+  window.addEventListener('scroll', function(){
+    
+    const posicoAtual= window.scrollY;
+    if(posicoAtual < alturaHero){
+      ocultaElementosDoHeader();
+    } else {
+      exibeElementosDoHeader();
+    }
 
+  })
+    function ocultaElementosDoHeader() {
+      const header =document.querySelector('header')
+      header.classList.add('header-is-hidden');
+    }
+    function exibeElementosDoHeader() {
+      const header =document.querySelector('header')
+      header.classList.remove('header-is-hidden');
+    }
   for (let i=0; i < buttons.length; i++){
     buttons[i].addEventListener('click', function(botao){
       const abaAlvo = botao.target.dataset.tabButton; 
@@ -12,8 +32,15 @@ document.addEventListener('DOMContentLoaded', function(){
 
     })
   }
-  
+  for (let i=0; i < quetions.length; i++){
+    quetions[i].addEventListener('click', abreOuFechaResposta);
+  }
 })
+function abreOuFechaResposta(elemento){
+  const classe ='faq__questions__item-is-open';
+  const elementopai = elemento.target.parentNode;
+  elementopai.classList.toggle(classe)
+}
 function removeBotaoAtivo() {
   const buttons = document.querySelectorAll('[data-tab-button]');
   for (let i=0; i < buttons.length; i++){
